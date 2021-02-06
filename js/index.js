58ac948af81bc99bb5af511726e4e8c9274e03b4
bsoares16 <70732971+bsoares16@users.noreply.github.com>
2021-02-06 02:35:49 -0500

    Hw 19 attempt1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 console.log("You index.js file is loaded correctly!");
+$( ".navitems ul li" ).hover(
+    function() {
+      $( this ).addClass( "hover" );
+    }, function() {
+      $( this ).removeClass( "hover" );
+    }
+  );
