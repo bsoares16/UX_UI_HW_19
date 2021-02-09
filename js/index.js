0135175cf2b1e50bc0877931aca379613b393897
bsoares16 <70732971+bsoares16@users.noreply.github.com>
2021-02-09 00:32:03 -0500

    Updated color and tried to add information to footer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 console.log("You index.js file is loaded correctly!");
-console.log("You index.js file is loaded correctly!");
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
